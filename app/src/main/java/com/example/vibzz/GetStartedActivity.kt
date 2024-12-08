f8a045cf290160807cd9a0b8d7bf7ddbe74e2394
rllakhan <rl.ramlakhan@outlook.com>
2024-12-08 17:23:16 +0530

    add getstarted screen

--- app/src/main/java/com/example/vibzz/GetStartedActivity.kt
+++ app/src/main/java/com/example/vibzz/GetStartedActivity.kt
@@ -1,12 +1,18 @@
 package com.example.vibzz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GetStartedActivity : AppCompatActivity() {
+    private lateinit var btnCreateAccount: Button
+    private lateinit var tvSignIn: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,21 @@ class GetStartedActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnCreateAccount = findViewById(R.id.btnCreateAccount)
+        tvSignIn = findViewById(R.id.tvSignIn)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this@GetStartedActivity, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        tvSignIn.setOnClickListener {
+            val intent = Intent(this@GetStartedActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
